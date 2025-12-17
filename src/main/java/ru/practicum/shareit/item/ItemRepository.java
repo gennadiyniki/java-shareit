abc5d54ada459ecd7b80f31b24e7644fa056a85d
gennadiyniki <nikitkogs@gmail.com>
2025-12-17 12:20:59 +0300

    ФЗ-15

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -8,17 +8,16 @@ import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-
     List<Item> findByOwnerId(Long ownerId);
 
     @Query("""
-    SELECT i 
-    FROM Item i 
-    WHERE i.available = true 
-    AND (
-        LOWER(i.name) LIKE LOWER(CONCAT('%', :text, '%')) 
-        OR LOWER(i.description) LIKE LOWER(CONCAT('%', :text, '%'))
-    )
-    """)
+        SELECT i
+        FROM Item i
+        WHERE i.available = true
+        AND (
+            LOWER(i.name) LIKE LOWER(CONCAT('%', :text, '%'))
+            OR LOWER(i.description) LIKE LOWER(CONCAT('%', :text, '%'))
+        )
+        """)
     List<Item> searchAvailableItems(@Param("text") String text);
 }
